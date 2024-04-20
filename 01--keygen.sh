35c0ed2f7e9896302c062ed7aa2f7f451c665054
fvsqr <48791253+fvsqr@users.noreply.github.com>
2024-04-20 23:27:34 +0200

    Update 01--keygen.sh

--- 01--keygen.sh
+++ 01--keygen.sh
@@ -1,3 +1,15 @@
 #! /bin/sh
 ssh-keygen -t ecdsa -q -f "$HOME/.ssh/id_ecdsa" -N ""
+
+echo "###############################################################"
+echo "# Copy the output to the clipboard and open a ssh session     #"
+echo "# from your local machine to the 'SSH Proxy Server. Open file #"
+echo "# ~/.ssh/authorized_keys                                      #"
+echo "# and append a line with the content from the clipboard.      #"
+echo "###############################################################"
+echo .
 cat "$HOME/.ssh/id_ecdsa.pub"
+echo .
+echo .
+echo "Press any key, when done..."
+read -rn1
