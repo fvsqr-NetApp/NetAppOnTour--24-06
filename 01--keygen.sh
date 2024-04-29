4e8aa48a06d49406a33159f59c626885dbb43104
fvsqr <48791253+fvsqr@users.noreply.github.com>
2024-04-29 16:45:49 +0200

    Update 01--keygen.sh

--- 01--keygen.sh
+++ 01--keygen.sh
@@ -48,21 +48,21 @@ deploy_tetris() {
 }
 
 do_install() {
-  ssh_key
-  read -p "Ready to continue? (Y/N): " confirm < /dev/tty
-  if [ "$confirm" != "${confirm#[Yy]}" ] ;then 
-    echo "ok, let's start the installation..."
-  else
-    echo "Installation aborted"
-    exit 1
-  fi
-  echo
-  read -p "IP of the SSH Proxy Server: " ip < /dev/tty
-  read -p "user name of the SSH Proxy Server: " user < /dev/tty
-  read -p "Remote port to use: " remoteport < /dev/tty
-  read -p "Local port to use: " localport < /dev/tty
+  #ssh_key
+  #read -p "Ready to continue? (Y/N): " confirm < /dev/tty
+  #if [ "$confirm" != "${confirm#[Yy]}" ] ;then 
+  #  echo "ok, let's start the installation..."
+  #else
+  #  echo "Installation aborted"
+  #  exit 1
+  #fi
+  #echo
+  #read -p "IP of the SSH Proxy Server: " ip < /dev/tty
+  #read -p "user name of the SSH Proxy Server: " user < /dev/tty
+  #read -p "Remote port to use: " remoteport < /dev/tty
+  #read -p "Local port to use: " localport < /dev/tty
   
-  ssh_tunnel $ip $user $remoteport $localport
+  #ssh_tunnel $ip $user $remoteport $localport
   
   deploy_tetris
 }
